0283cb466940ff9f490d4e65c3c9975ee39567c9
eshupps <eshupps@binarywave.com>
2015-08-26 09:45:15 -0500

    Initial commit

---
+++ Demo.JSOM.Library/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="a669ef6b-6b24-4a19-ba01-365d2cc48904" description="SharePoint App Feature" featureId="a669ef6b-6b24-4a19-ba01-365d2cc48904" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="Demo.JSOM.Library Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="b05c484e-660d-48a7-9f08-f98e1be0c91b" />
+    <projectItemReference itemId="0e9f1b27-038e-4660-9057-d062c31f2ef9" />
+    <projectItemReference itemId="880efba4-fb32-4694-896e-5c1fd6c1b155" />
+    <projectItemReference itemId="7c742c62-ebee-4561-ba1f-a323d55fddda" />
+  </projectItems>
+</feature>
---
+++ Demo.JSOM.Library/Scripts/App.js
@@ -0,0 +1,79 @@
+﻿'use strict';
+var appweburl;
+var hostweburl;
+var appContext;
+var hostContext;
+
+
+$(document).ready(function () {
+    hostweburl =
+            decodeURIComponent(
+                getQueryStringParameter("SPHostUrl")
+        );
+    appweburl =
+        decodeURIComponent(
+            getQueryStringParameter("SPAppWebUrl")
+    );
+
+    var scriptbase = hostweburl + "/_layouts/15/";
+
+    $.getScript(scriptbase + "SP.Runtime.js",
+        function () {
+            $.getScript(scriptbase + "SP.js",
+                function () {
+                    $.getScript(scriptbase + "SP.RequestExecutor.js", getUserName);
+            });
+        }
+    );
+});
+
+function getUserName() {
+    appContext = new SP.ClientContext(appweburl);
+    var factory = new SP.ProxyWebRequestExecutorFactory(appweburl);
+    appContext.set_webRequestExecutorFactory(factory);
+    hostContext = new SP.AppContextSite(appContext, hostweburl);
+    
+    var user = appContext.get_web().get_currentUser();
+    appContext.load(user);
+    appContext.executeQueryAsync(
+        function () {
+            $('#welcome').html('Welcome,  ' + user.get_title());
+        },
+        function (sender, args) {
+            alert(args.get_message())
+        }
+    );
+}
+
+function createLibrary() {
+    $("#message").attr("class","waiting").html("Working on it...");
+
+    var web = hostContext.get_web();
+    var lists = web.get_lists();
+    var title = $("#libraryName").val();
+    var listCreationInfo = new SP.ListCreationInformation();
+    listCreationInfo.set_title(title);
+    listCreationInfo.set_templateType(SP.ListTemplateType.documentLibrary);
+    lists.add(listCreationInfo);
+
+    appContext.load(lists);
+    appContext.executeQueryAsync(
+        function () {
+            $("#message").attr("class","success").html("Library creation succeeded.");
+        },
+        function (sender, args) {
+            $("#message").attr("class","error").html("Library creation failed.<br/>" + args.get_message());
+        }
+    );
+}
+
+function getQueryStringParameter(paramToRetrieve) {
+    var params =
+        document.URL.split("?")[1].split("&");
+    var strParams = "";
+    for (var i = 0; i < params.length; i = i + 1) {
+        var singleParam = params[i].split("=");
+        if (singleParam[0] == paramToRetrieve)
+            return singleParam[1];
+    }
+}
